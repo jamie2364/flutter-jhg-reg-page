00651960f6dc52242d453f6f23d234be3f18b0a0
jamie2364 <jamie2364@gmail.com>
2024-02-28 18:15:51 +0500

    changed 7 day trial text

--- lib/src/constant.dart
+++ lib/src/constant.dart
@@ -7,8 +7,8 @@ class Constant {
       "https://www.evolo.app/wp-json/myplugin/v1/jwt-check-subscription/";
   static const String marketingUrl =
       "https://app.bentonow.com/api/v1/batch/subscribers?site_uuid=f0eba21f0b6640bbbe47fedefa843b0f";
-    static const String checkSubscriptionUrl =
-       "https://www.jamieharrisonguitar.com/wp-json/myplugin/v1/jwt-check-subscription";
+  static const String checkSubscriptionUrl =
+      "https://www.jamieharrisonguitar.com/wp-json/myplugin/v1/jwt-check-subscription";
   static const String reportBugUrl =
       "https://www.jamieharrisonguitar.com/wp-json/custom/v1/report";
 
@@ -19,7 +19,8 @@ class Constant {
   static const String jamieUrl = 'https://www.Jamieharrisonguitar.com/';
   static const String evoloUrl = 'https://www.evolo.app/';
 
-  static const String evoloBaseUrl= 'https://www.evolo.app/wp-json/jwt-auth/v1/token';
+  static const String evoloBaseUrl =
+      'https://www.evolo.app/wp-json/jwt-auth/v1/token';
   static const String productIdEndPoint = 'wp-json/jhg-apps/v1/product-ids';
 
   static const String jamieHarrison = 'JAMIE HARRISON';
@@ -48,7 +49,7 @@ class Constant {
   static const String pleaseChoosePlan =
       'Please choose a plan below to get started';
   static const String annualPlan = 'Annual Plan';
-  static const String oneWeekFree = '1 week free, then ';
+  static const String oneWeekFree = '7 days free, then ';
   static const String perYear = 'per year';
   static const String perMonth = 'per month';
   static const String weeklySave = 'More Info';
@@ -63,11 +64,13 @@ class Constant {
   static const String restorePurchases = 'Restore Purchases';
   static const String williamShakespeare = 'William Shakespeare';
   static const String visitJamieHarrisonGuitar = 'Visit Jamie Harrison Guitar';
-  static const String renewMonthlyText = 'Renews automatically every month.\n Cancel anytime';
+  static const String renewMonthlyText =
+      'Renews automatically every month.\n Cancel anytime';
   static const String chooseYourSubscriptionText = 'Choose your subscription';
   static const String evoloUrlText = 'Evolo.app';
   static const String jamieUrlText = 'Jamieharrisonguitar.com';
-  static const String subscriptionUrlSubText ='Please choose the platform from the list\nbelow where you have your active subscription to Evolo';
+  static const String subscriptionUrlSubText =
+      'Please choose the platform from the list\nbelow where you have your active subscription to Evolo';
   static const String appQuotes =
       '“If music be the food of love, play on,\n my friend...”';
   static const String copyRight =
@@ -88,5 +91,6 @@ class Constant {
   static const String serverErrorMessage =
       'Sorry, we encountered an error. \n Please contact us here \n And link here to jamieharrisonguitar.com/support';
 
-  static const String productIdsFailedMessage = 'Sorry, Something went wrong, please try again.';
+  static const String productIdsFailedMessage =
+      'Sorry, Something went wrong, please try again.';
 }
