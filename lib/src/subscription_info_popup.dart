9495d1bd1d53fe660add3d615f23fbd944ee56ff
jamie2364 <jamie2364@gmail.com>
2024-04-19 15:06:34 +0200

    changed padding

--- lib/src/subscription_info_popup.dart
+++ lib/src/subscription_info_popup.dart
@@ -17,7 +17,7 @@ void showWeeklySaveInfoDialog(
         shadowColor: Theme.of(context).shadowColor,
         insetPadding: const EdgeInsets.symmetric(horizontal: 14, vertical: 32),
         contentPadding:
-            const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+            const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
         content: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
