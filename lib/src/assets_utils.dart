4176ab703ccc168c00e3545feca2550d6e2bb60f
Faizan Akram <48491556+faizanfiverr909@users.noreply.github.com>
2024-04-26 08:36:47 +0500

    removed assets from sub folder

--- lib/src/assets_utils.dart
+++ lib/src/assets_utils.dart
@@ -3,5 +3,5 @@ import 'dart:io';
 import 'package:reg_page/reg_page.dart';
 
 File getAsset(String path) => File(
-    "${StringsDownloadService().dir?.path}/assets/$path");
+    "${StringsDownloadService().dir?.path}/$path");
 
